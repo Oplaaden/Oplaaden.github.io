7ec004cdad8fce0fee625244491d993a09c4e871
Owen2424 <jason.newo24@gmail.com>
2021-12-27 14:13:34 +1100

    doing shit

--- translator.js
+++ translator.js
@@ -1,1 +1,328 @@
-var _0x493c=['206593PcbWQX','1057734vyytRi','removeAllRanges','English','selectNode','1762925ySiTWV','The\x203\x20characters\x20must\x20be\x20different\x20for\x20safety\x20purposes','Translate_laoshi','split','length','getElementById','replace','1859LatUyI','Must\x20translate\x20at\x20least\x203\x20characters\x20for\x20safety\x20purposes','getSelection','42842AcaTjk','489witbRS','createRange','1577hCWmsI','reverse','611790WZFgkw','2739rxbnxW','innerHTML','2YjVFrp','join','You\x20used\x20a\x20character\x20which\x20is\x20not\x20allowed\x20to\x20be\x20used','value','29xaZrTB','addRange','5QFPuNR','Error','push'];(function(_0xdf46b3,_0x5d4881){var _0x284531=_0x3ffa;while(!![]){try{var _0x45e953=parseInt(_0x284531(0x165))+parseInt(_0x284531(0x149))+-parseInt(_0x284531(0x159))*parseInt(_0x284531(0x154))+parseInt(_0x284531(0x15f))*-parseInt(_0x284531(0x153))+-parseInt(_0x284531(0x164))*parseInt(_0x284531(0x161))+parseInt(_0x284531(0x158))*-parseInt(_0x284531(0x15b))+-parseInt(_0x284531(0x150))*-parseInt(_0x284531(0x156));if(_0x45e953===_0x5d4881)break;else _0xdf46b3['push'](_0xdf46b3['shift']());}catch(_0xb733bc){_0xdf46b3['push'](_0xdf46b3['shift']());}}}(_0x493c,0xdf230));function fn1(){var _0x576b98=_0x3ffa,_0x24874d=document['getElementById'](_0x576b98(0x147))['value'];laoshi(_0x24874d);}function laoshi(_0x5a15c1){var _0x2877c1=_0x3ffa;_0x5a15c1=_0x5a15c1['split']('\x20')[_0x2877c1(0x15c)]('_');var _0x2320f5=['a','1','z','g','n','3','i','d','p','y','`','6','@','A','f','B','q','Q','b','!','C','2','R','m','j','D','_','.','E','S','^','F','h',',','/','<','G','T','*','(',')','9','H','>','$','U','%','7','u','V',':',';','I','k','0','#','W','o','?','J','e','X','c','K','8','Y','t','L','v','&','-','l','M','4','x','N','w','r','=','+','O','s','5','\x27','\x22','P','Z','a','1','z','g','n','3','i','d','p','y'],_0x315016=[],_0x2c7071=0x1,_0xe7ac3=_0x5a15c1[_0x2877c1(0x14d)];for(letter of _0x5a15c1){var _0x1f889e=0x0;for(Value of _0x2320f5){if(Value==letter){_0x2c7071+=0x1;var _0x3ddbaa=_0x1f889e,_0x2ada3e=_0x2c7071;if(_0x5a15c1[_0x2877c1(0x14d)]%0x3==0x0){if(_0x2ada3e%0x4==0x0||_0x2ada3e%0x3==0x0){var _0x14678e=_0x2320f5[_0x3ddbaa+0x3];_0x315016[_0x2877c1(0x163)](_0x14678e);break;}else{if(_0x2ada3e%0x2==0x0){var _0x14678e=_0x2320f5[_0x3ddbaa+0x1];_0x315016[_0x2877c1(0x163)](_0x14678e);break;}else{if(_0x2ada3e%0x5==0x0){var _0x14678e=_0x2320f5[_0x3ddbaa+0x4];_0x315016[_0x2877c1(0x163)](_0x14678e);break;}else{var _0x14678e=_0x2320f5[_0x3ddbaa+0x5];_0x315016['push'](_0x14678e);break;}}}}else{if(_0x5a15c1[_0x2877c1(0x14d)]%0x4==0x0){if(_0x2ada3e%0x2==0x0){var _0x14678e=_0x2320f5[_0x3ddbaa+0x6];_0x315016[_0x2877c1(0x163)](_0x14678e);break;}else{if(_0x2ada3e%0x3==0x0){var _0x14678e=_0x2320f5[_0x3ddbaa+0x2];_0x315016[_0x2877c1(0x163)](_0x14678e);break;}else{if(_0x2ada3e%0x5==0x0||_0x2ada3e%0x7==0x0){var _0x14678e=_0x2320f5[_0x3ddbaa+0x3];_0x315016['push'](_0x14678e);break;}else{var _0x14678e=_0x2320f5[_0x3ddbaa+0x1];_0x315016[_0x2877c1(0x163)](_0x14678e);break;}}}}else{if(_0x5a15c1[_0x2877c1(0x14d)]%0x3!=0x0){if(_0x2ada3e%0x4==0x0){var _0x14678e=_0x2320f5[_0x3ddbaa+0x4];_0x315016[_0x2877c1(0x163)](_0x14678e);break;}if(_0x2ada3e%0x2==0x0||_0x2ada3e%0x5==0x0){var _0x14678e=_0x2320f5[_0x3ddbaa+0x6];_0x315016[_0x2877c1(0x163)](_0x14678e);break;}else{if(_0x2ada3e%0x3==0x0){var _0x14678e=_0x2320f5[_0x3ddbaa+0x7];_0x315016[_0x2877c1(0x163)](_0x14678e);break;}else{var _0x14678e=_0x2320f5[_0x3ddbaa+0x2];_0x315016[_0x2877c1(0x163)](_0x14678e);break;}}}}}}else _0x1f889e+=0x1;}}_0x2c7071=0x1,_0x315016=_0x315016['join'](''),_0x315016=_0x315016[_0x2877c1(0x14f)](/_/g,'\x20');var _0x3df731=_0x315016[_0x2877c1(0x14d)];_0xe7ac3==_0x3df731?_0x315016[_0x2877c1(0x14d)]>=0x3?_0x315016['length']==0x3?_0x315016[0x0]==_0x315016[0x1]||_0x315016[0x1]==_0x315016[0x2]||_0x315016[0x0]==_0x315016[0x2]?document['getElementById'](_0x2877c1(0x162))['innerHTML']=_0x2877c1(0x14a):(document[_0x2877c1(0x14e)](_0x2877c1(0x162))['innerHTML']='',document['getElementById'](_0x2877c1(0x14b))[_0x2877c1(0x15a)]=_0x315016):(document[_0x2877c1(0x14e)](_0x2877c1(0x162))[_0x2877c1(0x15a)]='',document['getElementById'](_0x2877c1(0x14b))[_0x2877c1(0x15a)]=_0x315016):document[_0x2877c1(0x14e)](_0x2877c1(0x162))[_0x2877c1(0x15a)]=_0x2877c1(0x151):document[_0x2877c1(0x14e)](_0x2877c1(0x162))[_0x2877c1(0x15a)]=_0x2877c1(0x15d);}function reverse(_0x30eab5){var _0x394caa=_0x3ffa;return _0x30eab5[_0x394caa(0x14c)]('')[_0x394caa(0x157)]()[_0x394caa(0x15c)]('');}function _0x3ffa(_0x4759d5,_0x5ae6ec){return _0x3ffa=function(_0x493ce0,_0x3ffa4f){_0x493ce0=_0x493ce0-0x147;var _0x4fb6e7=_0x493c[_0x493ce0];return _0x4fb6e7;},_0x3ffa(_0x4759d5,_0x5ae6ec);}function english(_0x47ce8c){var _0x25eee1=_0x3ffa,_0x4f4b11=['a','1','z','g','n','3','i','d','p','y','`','6','@','A','f','B','q','Q','b','!','C','2','R','m','j','D','_','.','E','S','^','F','h',',','/','<','G','T','*','(',')','9','H','>','$','U','%','7','u','V',':',';','I','k','0','#','W','o','?','J','e','X','c','K','8','Y','t','L','v','&','-','l','M','4','x','N','w','r','=','+','O','s','5','\x27','\x22','P','Z','a','1','z','g','n','3','i','d','p','y'];_0x4f4b11=_0x4f4b11['reverse'](),_0x47ce8c=_0x47ce8c[_0x25eee1(0x14c)]('\x20')[_0x25eee1(0x15c)]('_');var _0x4c608d=[],_0x38750b=0x1,_0x180e15=_0x47ce8c[_0x25eee1(0x14d)];for(letter of _0x47ce8c){var _0x555126=0x0;for(Value of _0x4f4b11){if(Value==letter){_0x38750b+=0x1;var _0x58cc85=_0x555126,_0x34ed8c=_0x38750b;if(_0x47ce8c[_0x25eee1(0x14d)]%0x3==0x0){if(_0x34ed8c%0x4==0x0||_0x34ed8c%0x3==0x0){var _0x326111=_0x4f4b11[_0x58cc85+0x3];_0x4c608d[_0x25eee1(0x163)](_0x326111);break;}else{if(_0x34ed8c%0x2==0x0){var _0x326111=_0x4f4b11[_0x58cc85+0x1];_0x4c608d['push'](_0x326111);break;}else{if(_0x34ed8c%0x5==0x0){var _0x326111=_0x4f4b11[_0x58cc85+0x4];_0x4c608d[_0x25eee1(0x163)](_0x326111);break;}else{var _0x326111=_0x4f4b11[_0x58cc85+0x5];_0x4c608d[_0x25eee1(0x163)](_0x326111);break;}}}}else{if(_0x47ce8c['length']%0x4==0x0){if(_0x34ed8c%0x2==0x0){var _0x326111=_0x4f4b11[_0x58cc85+0x6];_0x4c608d[_0x25eee1(0x163)](_0x326111);break;}else{if(_0x34ed8c%0x3==0x0){var _0x326111=_0x4f4b11[_0x58cc85+0x2];_0x4c608d[_0x25eee1(0x163)](_0x326111);break;}else{if(_0x34ed8c%0x5==0x0||_0x34ed8c%0x7==0x0){var _0x326111=_0x4f4b11[_0x58cc85+0x3];_0x4c608d['push'](_0x326111);break;}else{var _0x326111=_0x4f4b11[_0x58cc85+0x1];_0x4c608d[_0x25eee1(0x163)](_0x326111);break;}}}}else{if(_0x47ce8c[_0x25eee1(0x14d)]%0x3!=0x0){if(_0x34ed8c%0x4==0x0){var _0x326111=_0x4f4b11[_0x58cc85+0x4];_0x4c608d[_0x25eee1(0x163)](_0x326111);break;}if(_0x34ed8c%0x2==0x0||_0x34ed8c%0x5==0x0){var _0x326111=_0x4f4b11[_0x58cc85+0x6];_0x4c608d[_0x25eee1(0x163)](_0x326111);break;}else{if(_0x34ed8c%0x3==0x0){var _0x326111=_0x4f4b11[_0x58cc85+0x7];_0x4c608d[_0x25eee1(0x163)](_0x326111);break;}else{var _0x326111=_0x4f4b11[_0x58cc85+0x2];_0x4c608d[_0x25eee1(0x163)](_0x326111);break;}}}}}}else _0x555126+=0x1;}}_0x38750b=0x1,_0x4c608d=_0x4c608d[_0x25eee1(0x15c)](''),_0x4c608d=_0x4c608d['replace'](/_/g,'\x20');var _0x439a63=_0x4c608d['length'];_0x180e15==_0x439a63?_0x4c608d[_0x25eee1(0x14d)]>=0x3?_0x4c608d['length']==0x3?_0x4c608d[0x0]==_0x4c608d[0x1]||_0x4c608d[0x1]==_0x4c608d[0x2]||_0x4c608d[0x0]==_0x4c608d[0x2]?document['getElementById']('Error')[_0x25eee1(0x15a)]='The\x203\x20characters\x20must\x20be\x20different\x20for\x20safety\x20purposes':(document[_0x25eee1(0x14e)](_0x25eee1(0x162))[_0x25eee1(0x15a)]='',document['getElementById'](_0x25eee1(0x14b))['innerHTML']=_0x4c608d):(document[_0x25eee1(0x14e)](_0x25eee1(0x162))[_0x25eee1(0x15a)]='',document[_0x25eee1(0x14e)](_0x25eee1(0x14b))['innerHTML']=_0x4c608d):document[_0x25eee1(0x14e)](_0x25eee1(0x162))['innerHTML']=_0x25eee1(0x151):document['getElementById'](_0x25eee1(0x162))[_0x25eee1(0x15a)]='You\x20used\x20a\x20character\x20which\x20is\x20not\x20allowed\x20to\x20be\x20used';}function fn2(){var _0x7f5c7f=_0x3ffa,_0x3b519a=document[_0x7f5c7f(0x14e)]('English')[_0x7f5c7f(0x15e)];english(_0x3b519a);}function copy(){var _0x445c80=_0x3ffa,_0x32f37d=document[_0x445c80(0x155)]();_0x32f37d[_0x445c80(0x148)](document['getElementById'](_0x445c80(0x14b))),window['getSelection']()[_0x445c80(0x166)](),window[_0x445c80(0x152)]()[_0x445c80(0x160)](_0x32f37d),document['execCommand']('copy'),window[_0x445c80(0x152)]()[_0x445c80(0x166)](),alert('Copied\x20to\x20clipboard');}
+function fn1()
+		{
+			var str = document.getElementById("English").value;
+			laoshi(str);
+		}
+function laoshi(thing)
+{	
+	if(thing == "ok foudfjdjfjxkdjs  you didjgxjgxgkfjskydiydyigchv bjcidseisutsutxjtf khciycougdoerrtyoftuskccmjzjsjskaortoufxlgxtjdukfuldyidhssjtfupgupvkhcmck gc")
+	{
+		Newarray = "This isn't an actual code, Ryan"
+		document.getElementById("Error").innerHTML = "";
+	  document.getElementById("Translate_laoshi").innerHTML = Newarray;
+	}
+	else{
+		thing = thing.split(" ").join("_");
+	// var letters = thing.replace(/[^a-z]+/g, '');
+	// thing = letters;
+	var array1 = ['a','1','z','g','n','3','i','d','p','y','`','6','@','A','f','B','q','Q','b','!','C','2','R','m','j','D','_','.','E','S','^','F','h',',','/','<','G','T','*','(',')','9','H','>','$','U','%','7','u','V',':',';','I','k','0','#','W','o','?','J','e','X','c','K','8','Y','t','L','v','&','-','l','M','4','x','N','w','r','=','+','O','s','5','\'',"\"",'P','Z','a','1','z','g','n','3','i','d','p','y'];
+	var Newarray = []
+	var shift = 1;
+	var length1 = thing.length;
+	
+	for(letter of thing)
+	{
+		var loopthrough = 0;
+		for(Value of array1)
+		{
+			if(Value == letter)
+			{
+				shift += 1;
+				var Bob = loopthrough;
+				var Thingy = shift;
+				if(thing.length % 3 == 0)
+				{
+					if(Thingy % 4 == 0 || Thingy % 3 == 0)
+					{	
+					var Newletter = array1[Bob + 3];
+					Newarray.push(Newletter);
+					break;
+					}
+					else if(Thingy % 2 == 0)
+					{
+					var Newletter = array1[Bob + 1];
+					Newarray.push(Newletter);
+					break;
+					}
+					else if(Thingy % 5 == 0)
+					{
+					var Newletter = array1[Bob + 4];
+					Newarray.push(Newletter);
+					break;
+					}
+					else
+					{
+					var Newletter = array1[Bob + 5];
+					Newarray.push(Newletter);
+					break;
+					}
+				}
+				else if(thing.length % 4 == 0)
+				{
+					if(Thingy % 2 == 0)
+					{	
+					var Newletter = array1[Bob + 6];
+					Newarray.push(Newletter);
+					break;
+					}
+					else if(Thingy % 3 == 0)
+					{
+					var Newletter = array1[Bob + 2];
+					Newarray.push(Newletter);
+					break;
+					}
+					else if(Thingy % 5 == 0 || Thingy % 7 == 0 )
+					{
+					var Newletter = array1[Bob + 3];
+					Newarray.push(Newletter);
+					break;
+					}
+					else
+					{
+					var Newletter = array1[Bob + 1];
+					Newarray.push(Newletter);
+					break;
+					}
+				}
+				else if(thing.length % 3 != 0)
+				{
+					if(Thingy % 4 == 0)
+					{	
+					var Newletter = array1[Bob + 4];
+					Newarray.push(Newletter);
+					break;
+					}
+					if(Thingy % 2 == 0 || Thingy % 5 == 0)
+					{
+					var Newletter = array1[Bob + 6];
+					Newarray.push(Newletter);
+					break;
+					}
+					else if(Thingy % 3 == 0)
+					{
+					var Newletter = array1[Bob + 7];
+					Newarray.push(Newletter);
+					break;
+					}
+					else
+					{
+					var Newletter = array1[Bob + 2];
+					Newarray.push(Newletter);
+					break;
+					}
+				}
+			}
+			else
+			{
+				loopthrough += 1;		
+			}
+		}
+	}
+	shift = 1;
+  	Newarray = Newarray.join("");
+  	Newarray = Newarray.replace(/_/g, ' ');
+  	var length2 = Newarray.length;
+ 
+
+	if(length1 == length2)
+  	{
+  		if(Newarray.length >= 3)
+  		{
+  		if(Newarray.length == 3)
+  		{
+  			if(Newarray[0] == Newarray[1] || Newarray[1] == Newarray[2] || Newarray[0] == Newarray[2])
+  			{
+  				document.getElementById("Error").innerHTML = "The 3 characters must be different for safety purposes"; 			
+  			}
+	  		else
+	  		{
+	  			document.getElementById("Error").innerHTML = "";
+	  			document.getElementById("Translate_laoshi").innerHTML = Newarray;
+  			}
+  		}
+  		else
+  		{
+  			document.getElementById("Error").innerHTML = "";
+	  		document.getElementById("Translate_laoshi").innerHTML = Newarray;
+  		}
+  		
+	  	}
+		else
+		{
+			document.getElementById("Error").innerHTML = "Must translate at least 3 characters for safety purposes";
+		}
+	 }
+	 else
+	 {
+	 	document.getElementById("Error").innerHTML = "You used a character which is not allowed to be used";
+	 }
+	}
+	
+}
+function reverse(s){
+    return s.split("").reverse().join("");
+}
+function english(thing)
+{
+	var array1 = ['a','1','z','g','n','3','i','d','p','y','`','6','@','A','f','B','q','Q','b','!','C','2','R','m','j','D','_','.','E','S','^','F','h',',','/','<','G','T','*','(',')','9','H','>','$','U','%','7','u','V',':',';','I','k','0','#','W','o','?','J','e','X','c','K','8','Y','t','L','v','&','-','l','M','4','x','N','w','r','=','+','O','s','5','\'',"\"",'P','Z','a','1','z','g','n','3','i','d','p','y'];
+	array1 = array1.reverse();
+
+	thing = thing.split(" ").join("_");
+	// var letters = thing.replace(/[^a-z]+/g, '');
+	// thing = letters;
+	var Newarray = [];
+	var shift = 1;
+	var length1 = thing.length;
+
+	for(letter of thing)
+	{
+		var loopthrough = 0;
+		for(Value of array1)
+		{
+			if(Value == letter)
+			{
+				shift += 1;
+				var Bob = loopthrough;
+				var Thingy = shift;
+				if(thing.length % 3 == 0)
+				{
+					if(Thingy % 4 == 0 || Thingy % 3 == 0)
+					{	
+					var Newletter = array1[Bob + 3];
+					Newarray.push(Newletter);
+					break;
+					}
+					else if(Thingy % 2 == 0)
+					{
+					var Newletter = array1[Bob + 1];
+					Newarray.push(Newletter);
+					break;
+					}
+					else if(Thingy % 5 == 0)
+					{
+					var Newletter = array1[Bob + 4];
+					Newarray.push(Newletter);
+					break;
+					}
+					else
+					{
+					var Newletter = array1[Bob + 5];
+					Newarray.push(Newletter);
+					break;
+					}
+				}
+				else if(thing.length % 4 == 0)
+				{
+					if(Thingy % 2 == 0)
+					{	
+					var Newletter = array1[Bob + 6];
+					Newarray.push(Newletter);
+					break;
+					}
+					else if(Thingy % 3 == 0)
+					{
+					var Newletter = array1[Bob + 2];
+					Newarray.push(Newletter);
+					break;
+					}
+					else if(Thingy % 5 == 0 || Thingy % 7 == 0 )
+					{
+					var Newletter = array1[Bob + 3];
+					Newarray.push(Newletter);
+					break;
+					}
+					else
+					{
+					var Newletter = array1[Bob + 1];
+					Newarray.push(Newletter);
+					break;
+					}
+				}
+				else if(thing.length % 3 != 0)
+				{
+					if(Thingy % 4 == 0)
+					{	
+					var Newletter = array1[Bob + 4];
+					Newarray.push(Newletter);
+					break;
+					}
+					if(Thingy % 2 == 0 || Thingy % 5 == 0)
+					{
+					var Newletter = array1[Bob + 6];
+					Newarray.push(Newletter);
+					break;
+					}
+					else if(Thingy % 3 == 0)
+					{
+					var Newletter = array1[Bob + 7];
+					Newarray.push(Newletter);
+					break;
+					}
+					else
+					{
+					var Newletter = array1[Bob + 2];
+					Newarray.push(Newletter);
+					break;
+					}
+				}
+			}
+			else
+			{
+				loopthrough += 1;		
+			}
+		}
+	}
+	shift = 1;
+  	Newarray = Newarray.join("");
+  	Newarray = Newarray.replace(/_/g, ' ');
+  	var length2 = Newarray.length;
+  	if(length1 == length2)
+  	{
+  		if(Newarray.length >= 3)
+  		{
+  		if(Newarray.length == 3)
+  		{
+  			if(Newarray[0] == Newarray[1] || Newarray[1] == Newarray[2] || Newarray[0] == Newarray[2])
+  			{
+  				document.getElementById("Error").innerHTML = "The 3 characters must be different for safety purposes"; 			
+  			}
+	  		else
+	  		{
+	  			document.getElementById("Error").innerHTML = "";
+	  			document.getElementById("Translate_laoshi").innerHTML = Newarray;
+  			}
+  		}
+  		else
+  		{
+  			document.getElementById("Error").innerHTML = "";
+	  		document.getElementById("Translate_laoshi").innerHTML = Newarray;
+  		}
+  		
+	  	}
+		else
+		{
+			document.getElementById("Error").innerHTML = "Must translate at least 3 characters for safety purposes";
+		}
+	 }
+	 else
+	 {
+	 	document.getElementById("Error").innerHTML = "You used a character which is not allowed to be used";
+	 }
+}
+function fn2()
+{
+	var str = document.getElementById("English").value;
+
+	english(str);
+}
+
+function copy() {
+  /* Get the text field */
+  var r = document.createRange();
+  r.selectNode(document.getElementById("Translate_laoshi"));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+  alert("Copied to clipboard")
+}
